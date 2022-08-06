696b5a9d0107dac0b1671ebd892713d0bcd26388
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-08-06 15:04:37 -0500

    Lecture 47

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <h1>Homepage</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil atque obcaecati ipsa nostrum. Laudantium consequuntur architecto excepturi fugit qui perferendis necessitatibus cum accusamus facilis repellat error odio molestias, vero, tenetur cupiditate? Tenetur, eum doloremque, sequi nesciunt dolor nobis reprehenderit minus adipisci eveniet excepturi rem animi voluptas sunt harum unde. Veniam officia commodi aperiam maxime natus voluptatum ratione perspiciatis quis iste perferendis! Laboriosam itaque aliquam veniam, suscipit nesciunt voluptas ullam minus doloremque nemo, fuga error excepturi facilis tempore porro animi iusto. Vero iure laudantium provident ducimus quaerat ullam facere exercitationem corrupti possimus laborum id accusantium error neque, voluptatibus debitis, veritatis quam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil atque obcaecati ipsa nostrum. Laudantium consequuntur architecto excepturi fugit qui perferendis necessitatibus cum accusamus facilis repellat error odio molestias, vero, tenetur cupiditate? Tenetur, eum doloremque, sequi nesciunt dolor nobis reprehenderit minus adipisci eveniet excepturi rem animi voluptas sunt harum unde. Veniam officia commodi aperiam maxime natus voluptatum ratione perspiciatis quis iste perferendis! Laboriosam itaque aliquam veniam, suscipit nesciunt voluptas ullam minus doloremque nemo, fuga error excepturi facilis tempore porro animi iusto. Vero iure laudantium provident ducimus quaerat ullam facere exercitationem corrupti possimus laborum id accusantium error neque, voluptatibus debitis, veritatis quam.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
